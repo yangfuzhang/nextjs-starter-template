e157f6f68e4a87eb353aa4b8acef8bab6493a90a
yangfuzhang <yangfuzhang0720@126.com>
2025-06-28 20:59:52 +0800

    [fix] 修改拼写错误

--- data/prompts.ts
+++ data/prompts.ts
@@ -1,4 +1,4 @@
-export const privacy_polick = `
+export const privacy_policy = `
   update privacy-policy according to landing page content @en.json
   with brand name "Your brand name", domain "yourdomain.com", contact email is "youremail@example.com"
 `;
