068fddf8dde4285ad0d8c845cd456ff1570b6ba2
yangfuzhang <yangfuzhang0720@126.com>
2025-06-29 13:33:50 +0800

    [feat] middleware完善

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,14 @@
+import { auth } from "@/auth";
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export default auth((req) => {
+  return intlMiddleware(req);
+});
 
 export const config = {
   matcher:
-    "/((?!api|auth|admin|_next|robots.txt|sitemap.xml|favicon.ico|.*\\..*).*)",
+    "/((?!api|auth|admin|_next/static|_next/image|robots.txt|sitemap.xml|favicon.ico|.*\\..*).*)",
 };
-
-
