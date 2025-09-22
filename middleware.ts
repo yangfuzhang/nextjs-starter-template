bec40a675997c7fae7d79d02f119d010829d5202
yangfuzhang <yangfuzhang0720@126.com>
2025-09-23 01:33:17 +0800

    [feat] 更新middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,15 @@
-import { auth } from "@/auth";
+import type { NextRequest } from "next/server";
+import { updateSession } from "@/lib/supabase/middleware";
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
-const intlMiddleware = createMiddleware(routing);
+const handleI18nRouting = createMiddleware(routing);
 
-export default auth((req) => {
-  return intlMiddleware(req);
-});
+export default async function middleware(request: NextRequest) {
+  const response = handleI18nRouting(request);
+
+  return await updateSession(request, response);
+}
 
 export const config = {
   matcher:
