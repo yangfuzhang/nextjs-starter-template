21ee0d7c7a5e4eeab758d0b6213c11b176a57837
yangfuzhang <yangfuzhang0720@126.com>
2025-09-22 16:44:45 +0800

    [feat] add sharp and standalone mode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import withPlaiceholder from "@plaiceholder/next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "standalone",
+};
 
 const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+export default withNextIntl(withPlaiceholder(nextConfig));
