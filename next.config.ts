9f82e298d92fa042ae622b63a7c10167b95a2a37
yangfuzhang <yangfuzhang0720@126.com>
2025-09-27 17:05:48 +0800

    [feat] 修改next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import withPlaiceholder from "@plaiceholder/next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
 const withNextIntl = createNextIntlPlugin();
