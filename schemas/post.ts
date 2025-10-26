b734c218b9b08e65a4c57448dc01efb732e685b0
yangfuzhang <yangfuzhang0720@126.com>
2025-10-27 03:19:39 +0800

    [feat] 完善zod schema

--- schemas/post.ts
+++ schemas/post.ts
@@ -6,14 +6,14 @@ export const formSchema = z.object({
     .string({ error: "请输入文章标题" })
     .trim()
     .pipe(z.string().min(1, { message: "请输入文章标题" })),
-  slug: z.string().optional(),
+  slug: z.string().trim().optional(),
   cover: z
     .string({ error: "请输入文章封面图片url" })
     .trim()
     .pipe(z.string().min(1, { message: "请输入文章封面图片url" })),
-  abstract: z.string().optional(),
+  abstract: z.string().trim().optional(),
   content: z.string().optional(),
   published: z.boolean().optional(),
-  seo_title: z.string().optional(),
-  seo_description: z.string().optional(),
+  seo_title: z.string().trim().optional(),
+  seo_description: z.string().trim().optional(),
 });
