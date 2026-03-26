9caa8bbf4d0cfbf959f2a49bca9ab448dc936086
yangfuzhang <yangfuzhang0720@126.com>
2026-03-26 18:17:03 +0800

    [fix] 修复sql语句错误

--- data/profile.sql
+++ data/profile.sql
@@ -5,7 +5,7 @@ CREATE TABLE public.profiles (
     email TEXT,
     image TEXT,
     created_at timestamp with time zone not null default now(),
-    updated_at timestamp with time zone not null default now(),
+    updated_at timestamp with time zone not null default now()
 );
 
 -- Create a function to update the updated_at timestamp
